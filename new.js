a3302a1712d0b246532cd93ab8c327843b81bb08
David <50714711+schnavy@users.noreply.github.com>
2022-04-20 19:28:07 +0200

    frames

--- new.js
+++ new.js
@@ -5,6 +5,7 @@ const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 
 const FRAME_COUNT = 29
+// const MAX_FRAMES = 2000
 const SPEED = 50
 
 function pad (num) {
@@ -13,7 +14,8 @@ function pad (num) {
 }
 
 function currentFrame (num) {
-  return 'public/01_web/frame' + pad(num) + '.jpg'
+  console.log(pad(num % FRAME_COUNT));
+  return 'public/01_web/frame' + pad(num % FRAME_COUNT +1) + '.jpg'
 }
 
 
@@ -41,26 +43,6 @@ const updateImage = index => {
 }
 
 
-
-
-document.addEventListener('scroll', event => {
-  if (checkScrollDirectionIsUp(event)) {
-    counter-= 1+SPEED/10
-    if (counter < 0) {
-      counter = FRAME_COUNT
-    }
-  } else {
-    counter+= 1+SPEED/10
-    if (counter > FRAME_COUNT) {
-      counter = 0
-    }
-  }
-
-
-  requestAnimationFrame(() => updateImage(Math.floor(counter)))
-
-})
-
 function checkScrollDirectionIsUp (event) {
   if (event.wheelDelta) {
     return event.wheelDelta > 0
@@ -68,71 +50,37 @@ function checkScrollDirectionIsUp (event) {
   return event.deltaY < 0
 }
 
+
+document.addEventListener('scroll', event => {
+  // if (checkScrollDirectionIsUp(event)) {
+  //   counter-= 1+SPEED/10
+  //   if (counter < 0) {
+  //     counter = FRAME_COUNT
+  //   }
+  // } else {
+  //   counter+= 1+SPEED/10
+  //   if (counter > FRAME_COUNT) {
+  //     counter = 0
+  //   }
+  // }
+
+  const scrollTop = html.scrollTop;
+  // const maxScrollTop = html.scrollHeight - window.innerHeight;
+  // const scrollFraction = scrollTop / maxScrollTop;
+  // const frameIndex = Math.floor(scrollFraction * maxScrollTop)
+
+  const tmp = scrollTop *0.1
+  
+  counter = Math.floor(tmp)
+  
+
+
+
+
+console.log(counter);
+
+  requestAnimationFrame(() => updateImage(counter))
+})
+
+
 preloadImages()
-
-// var currentVideoIndex = 0
-// var listOfVideoSources = [
-//   // 'http://umlautfilms.de/wp-content/uploads/2022/01/Komp-1_1_2-1.mp4',
-//   // 'http://umlautfilms.de/wp-content/uploads/2022/01/Komp-1_1_3-1.mp4',
-//   // 'http://umlautfilms.de/wp-content/uploads/2022/01/Komp-1_1_5-1.mp4'
-//   // 'https://umlautfilms.de/wp-content/uploads/2022/01/Type_1_desk.mp4',
-//   // 'https://umlautfilms.de/wp-content/uploads/2022/01/Type_2_desk.mp4',
-//   // 'https://umlautfilms.de/wp-content/uploads/2022/01/Type_3_desk.mp4',
-
-//   'webm/Chrome_ImF.webm',
-//   'webm/Type_1_desk.webm',
-//   'webm/Type_2_desk.webm',
-//   'webm/Type_3_desk.webm'
-// ]
-
-// let vid = document.querySelector('.cover-region .background-video video')
-// let source = document.querySelector(
-//   '.cover-region .background-video video source'
-// )
-// let frameNumber = 0 // start video at frame 0
-// // lower numbers = faster playback
-// let playbackConst = 1000
-
-// let height = document.querySelector(".lay-content").clientHeight
-
-// window.onload = () => {
-
-//   currentVideoIndex = getRandomIndex()
-//   loadVideo(currentVideoIndex)
-
-// }
-
-// // dynamically set the page height according to video length
-// vid.addEventListener('loadedmetadata', function () {
-//   duration = vid.duration
-//   playbackConst = height / duration
-//   window.requestAnimationFrame(scrollPlay)
-
-// })
-
-// // video.onended = function(e) {
-// //   console.log("ende");
-// //   currentVideoIndex = getRandomIndex()
-// //   loadVideo(currentVideoIndex)
-// // };
-
-// function scrollPlay(){
-//   var frameNumber  = window.pageYOffset/playbackConst;
-//   vid.currentTime  = frameNumber;
-//   window.requestAnimationFrame(scrollPlay);
-// }
-
-// function loadVideo (currentVideoIndex) {
-//   source.setAttribute('src', listOfVideoSources[currentVideoIndex])
-//   vid.load()
-// }
-
-// function next() {
-//   currentVideoIndex = getRandomIndex()
-//   loadVideo(currentVideoIndex)
-// }
-
-// function getRandomIndex () {
-//   let r = Math.floor(listOfVideoSources.length * Math.random())
-//   return r
-// }

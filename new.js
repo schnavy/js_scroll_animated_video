233ff561dbb761e5cd2d4e1a3a0781abbfefa808
David <50714711+schnavy@users.noreply.github.com>
2022-06-24 14:46:03 +0200

    generalized

--- new.js
+++ new.js
@@ -1,46 +1,41 @@
+// ------ CONFIG ------
+const FRAME_DELAY = 2 // höhere Zahl = niederigere Frame Rate
+const SPEED = 10 // höhere Zahl = schneller
+
+const FILE_PREFIX = "public/frames_m/Echo-Umlaut_m_"
+const FILE_PREFIX_MOBILE = "public/frames_mobil/Echo-Umlaut_mobile_"
+
+// Init
 const IS_TOUCH = 'ontouchstart' in window || navigator.msMaxTouchPoints > 0
 let IS_MOBILE = window.innerWidth <= 1000
-
 const FRAME_COUNT = 29
-const SPEED = 30
 let counter = 0
 let size = updateSize()
+let frc = 0
+
+
+//PRELOADER
+
+images = []
+const preloadImages = () => {
+  for (let i = 1; i < FRAME_COUNT; i++) {
+    images[i] = new Image()
+    images[i].src = currentFrame(i)
+  }
+}
+
+preloadImages()
 
 const html = document.documentElement
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 
-function pad (num) {
-  var s = '000000000' + num
-  return s.substr(s.length - 5)
-}
-
-function currentFrame (num) {
-  console.log(pad(num % FRAME_COUNT))
-  if (IS_TOUCH) {
-    num = (num % FRAME_COUNT) + 1
-  }
-  if (IS_MOBILE) {
-    
-    return 'https://umlautfilms.de/wp-content/uploads/2022/06/Echo-Umlaut_mobile_' + pad(num) + '.jpg'
-  } else {
-    return 'https://umlautfilms.de/wp-content/uploads/2022/06/Echo-Umlaut_' + pad(num) + '.jpg'
-  }
-}
-
-const preloadImages = () => {
-  for (let i = 1; i < FRAME_COUNT; i++) {
-    const img = new Image()
-    img.src = currentFrame(i)
-  }
-}
-
-const img = new Image()
+let img = new Image()
 img.src = currentFrame(0)
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+
 img.onload = function () {
-  let size = Math.max(window.innerWidth, window.innerHeight)
   context.drawImage(
     img,
     canvas.width / 2 - size / 2,
@@ -50,8 +45,55 @@ img.onload = function () {
   )
 }
 
+document.addEventListener('resize', e => {
+  size = updateSize()
+})
+
+if (IS_TOUCH) {
+  document.addEventListener('scroll', event => {
+    const scrollTop = html.scrollTop
+    const tmp = scrollTop * 0.1
+
+    counter = Math.floor(tmp % FRAME_COUNT)
+    requestAnimationFrame(() => updateImage(counter))
+  })
+} else {
+  document.addEventListener('wheel', event => {
+    if (frc++ % FRAME_DELAY != 0) {
+      if (checkScrollDirectionIsUp(event)) {
+        counter -= Math.floor(1 + SPEED / 10)
+        if (counter < 0) {
+          counter = FRAME_COUNT - 1
+        }
+      } else {
+        counter += Math.floor(1 + SPEED / 10)
+        if (counter >= FRAME_COUNT) {
+          counter = 0
+        }
+      }
+      requestAnimationFrame(() => updateImage(counter))
+    }
+  })
+}
+
+function pad (num) {
+  var s = '000000000' + num
+  return s.substring(s.length - 5, s.length)
+}
+
+function currentFrame (num) {
+  console.log(pad(num % FRAME_COUNT))
+  if (IS_TOUCH) {
+    num = num % FRAME_COUNT
+  }
+  if (IS_MOBILE) {
+     return FILE_PREFIX_MOBILE + pad(num) + '.jpg'
+  } else {
+      return FILE_PREFIX + pad(num) + '.jpg'
+  }
+}
+
 const updateImage = index => {
-  let size = Math.max(window.innerWidth, window.innerHeight)
   img.src = currentFrame(index)
   context.drawImage(
     img,
@@ -76,39 +118,3 @@ function checkScrollDirectionIsUp (event) {
   }
   return event.deltaY < 0
 }
-
-document.addEventListener('resize', e => {
-  size = updateSize()
-})
-
-
-if (IS_TOUCH) {
-
-  document.addEventListener('scroll', event => {
-    const scrollTop = html.scrollTop
-    const tmp = scrollTop * 0.1
-
-    counter = Math.floor(tmp % FRAME_COUNT)
-    requestAnimationFrame(() => updateImage(counter))
-  })
-
-} else {
-
-  document.addEventListener('wheel', event => {
-    if (checkScrollDirectionIsUp(event)) {
-      counter -= 1 + SPEED / 10
-      if (counter < 0) {
-        counter = FRAME_COUNT -1
-      }
-    } else {
-      counter += 1 + SPEED / 10
-      if (counter >= FRAME_COUNT) {
-        counter = 0
-      }
-    }
-    requestAnimationFrame(() => updateImage(counter))
-  })
-
-}
-
-preloadImages()
